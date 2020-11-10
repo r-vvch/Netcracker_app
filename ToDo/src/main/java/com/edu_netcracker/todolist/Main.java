1f4d21e5bc3610758ce1ed0c985cf4ab994cd9ae
Roman Vovchenko <vovchenko.ra@phystech.edu>
2020-11-10 13:56:49 +0300

    final Spring app

--- ToDo/src/main/java/com/edu_netcracker/todolist/Main.java
+++ ToDo/src/main/java/com/edu_netcracker/todolist/Main.java
@@ -1,7 +1,6 @@
 package com.edu_netcracker.todolist;
 
 import com.edu_netcracker.todolist.services.AppService;
-import com.edu_netcracker.todolist.services.impl.AppConsole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Scanner;
