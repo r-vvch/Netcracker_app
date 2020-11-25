d596bd058ef6ed4b50ecd83c6bf19829b65c54dd
Roman Vovchenko <vovchenko.ra@phystech.edu>
2020-11-25 06:44:23 +0300

    what a fine looking website!

--- front/src/components/Search.js
+++ front/src/components/Search.js
@@ -1,11 +1,24 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import Search from "./Search";
 
-const Search = () => {
-    const text = "Search"
+const SearchNew = () => {
+    const amp = <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-search" fill="currentColor"
+                     xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd"
+              d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+        <path fill-rule="evenodd"
+              d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+    </svg>
+
     return (
-        <input placeholder={text}/>
+        <div className="input-group mb-3">
+            <div className="input-group-prepend">
+                <button className="btn btn-primary" type="button" id="button-addon1">{amp}</button>
+            </div>
+            <input type="text" className="form-control" placeholder="Search" aria-label="Example text with button addon"
+                   aria-describedby="button-addon1"/>
+        </div>
     )
 }
 
-export default Search;
+export default SearchNew;
--- front/src/components/ToDoList.js
+++ front/src/components/ToDoList.js
@@ -1,18 +1,27 @@
-import React from 'react';
+    import React from 'react';
 // import ReactDOM from 'react-dom';
 
 import Task from "./Task";
 
 const ToDoList = ({ toDoList }) => {
     const tasks = toDoList.map((item) => {
-        return (
-            <li key = {item.key}>
-                <Task name={item.name} isImportant={item.isImportant} />
-            </li>
-        )
+        if (item.isImportant) {
+            return (
+                <li class="list-group-item d-flex justify-content-between align-items-center" key={item.key}>
+                    <Task name={item.name} isImportant={item.isImportant}/>
+                    <span className="badge badge-primary badge-pill">!</span>
+                </li>
+            )
+        } else {
+            return (
+                    <li className="list-group-item" key={item.key}>
+                        <Task name={item.name} isImportant={item.isImportant}/>
+                    </li>
+                )
+        }
     })
     return (
-        <ul>
+        <ul class="list-group">
             {tasks}
         </ul>
     )
